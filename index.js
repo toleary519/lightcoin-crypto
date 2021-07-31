718d69a9881f828335014d66a004f6eb0d794b85
vagrant <t.oleary@me.com>
2021-07-31 04:22:37 +0000

    still need to impliment the strech but refactored

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    this.balance = 0;
+  }
 
-  constructor(amount) {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value
+    this.account.addTransaction(this);
   }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 }
 
 
+// for (let Transaction in myAccount.transactions) {
+//   balance += Transaction.amount
+// }
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+console.log(`Starting balance: ${myAccount.balance}`);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+
+// console.log(myAccount.transactions);
+console.log(`Balance: ${myAccount.balance}`);
